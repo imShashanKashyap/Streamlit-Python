f5ef6677e0eaaa3e5987f0d4fdd0cab87ebb686e
shashank01123 <shashank01123@gmail.com>
2023-08-05 21:48:37 +0530

    a

--- simple_app.py
+++ simple_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
 import requests
+import sqlite3
+
+# Define the name of the SQLite database
+DATABASE_NAME = "user_inputs.db"
+
+def init_db():
+    """Initialize the database and create table if not exists."""
+    with sqlite3.connect(DATABASE_NAME) as conn:
+        conn.execute(
+            '''CREATE TABLE IF NOT EXISTS user_inputs (name TEXT)'''
+        )
+        conn.commit()
+
+def insert_input_to_db(input_value):
+    """Insert the provided input value into the database."""
+    with sqlite3.connect(DATABASE_NAME) as conn:
+        conn.execute(
+            '''INSERT INTO user_inputs (name) VALUES (?)''', (input_value,)
+        )
+        conn.commit()
 
 def fetch_data_from_api(input_value):
     # Make a request to the API with user input as a parameter
@@ -10,21 +30,24 @@ def fetch_data_from_api(input_value):
         data = response.json()
         output = f'''{input_value.capitalize()} is a {str(data['gender']).upper()}\n 
         Confidence: {data['probability']*100} %'''
-    
         return output
     else:
         st.error("Error retrieving data from API")
         return None
 
 def main():
+    # Initialize the database and table
+    init_db()
     st.title("A simple Python based Web Application built using Streamlit created by Shashank Kashyap")
-    # st.write("Welcome to this FREE API-powered web application created by Shashank Kashyap")
     st.write('Visit the link to know more about me: https://imshashankashyap.github.io')
     
     # Get user input
     user_input = st.text_input("Enter any name to check its Gender")
     
     if user_input:  # Check if user_input is not empty
+        # Store the user input in the database
+        insert_input_to_db(user_input)
+        
         # Fetch data from the API based on user input
         data = fetch_data_from_api(user_input)
         
