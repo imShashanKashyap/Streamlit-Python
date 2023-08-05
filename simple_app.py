f4eeb812bdd0f0524dd2d457ce4a117463143710
shashank01123 <shashank01123@gmail.com>
2023-08-05 22:34:54 +0530

    aw

--- simple_app.py
+++ simple_app.py
@@ -38,7 +38,7 @@ def fetch_data_from_api(input_value):
 def main():
     # Initialize the database and table
     init_db()
-    st.title('Predict Gender by Name\n')
+    st.title('"Predict Gender by Name"\n')
     st.title("A Simple Python based Web Application built using Streamlit created by Shashank Kashyap")
     st.write('Visit the link to know more about me -> https://imshashankashyap.github.io')
     
